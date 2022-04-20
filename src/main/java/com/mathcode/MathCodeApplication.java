6bfb507cdb8e663fe956642bf2b384b206f23507
Григорий <52451620+GrigoriyVS@users.noreply.github.com>
2022-04-21 01:06:20 +0300

    add highlight and enter key

--- src/main/java/com/mathcode/MathCodeApplication.java
+++ src/main/java/com/mathcode/MathCodeApplication.java
@@ -1,7 +1,12 @@
 package com.mathcode;
 
+import org.apache.catalina.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class MathCodeApplication {
@@ -10,4 +15,10 @@ public class MathCodeApplication {
         SpringApplication.run(MathCodeApplication.class, args);
     }
 
+    @RequestMapping(value = "/createUser", method = RequestMethod.POST)
+    @ResponseBody
+    public User createUser(@RequestBody User user) {
+        System.out.println(user);
+        return user;
+    }
 }
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,255 +1,25 @@
-const HL_RULES = [
-    { cl: 'str',  re: /("[^"]+")/ },
-    { cl: 'optr', re: /^(var|let|for|in|of|function|function*|const|do|while|break|continue|if|else|switch|case|default|try|catch|throw|delete|void|yield|yield*|return)$/ },
-    { cl: 'cnst', re: /(\d+)/ },
-    { cl: 'brcs', re: /({|})/ }
-];
-///(?<=[=|\s])\d+(?=[;|\s])|\d+$/g
-
-let codeAreas_HTMLCollection = null;
-let codeAreas = [];
-let btn = null;
 window.onload=function(){
-    codeAreas_HTMLCollection = document.getElementsByClassName("codearea");
-    btn = document.getElementById('hl');
-    for (var codeArea of codeAreas_HTMLCollection){
-        codeArea.addEventListener('keyup', timeToColor);
-        codeAreas.push(codeArea)
-    }
-    timeToColor()
-}
-function timeToColor(e) {
-    console.clear()
-    alertNum("timeToColor")
-    for(let i = 0; i<codeAreas.length; i++) {
-        simpleHighlighter(getCodeArea({index:i}));
-    }
-    alertNum("timeToColor END")
-}
-
-function getCodeArea({findActive = false, index = -1} = {}){
-
-    if (!findActive && index <0) {//ошибка
-        alertNum("invalidArgumentException: Если не указан поиск активной области, то должен быть указан корректный индекс.")
-        return
-    }
-    for (var codeArea of codeAreas){
-        if (findActive && document.activeElement == codeArea||//активная область
-            !findActive && codeAreas.indexOf(codeArea) == index) {//по индексу
-            return codeArea;
-        }
-    }
-    return undefined;
-}
-
-
-let isEnterBt = false
-let flag = "_±FLAG±_"
-function setFlag(el){
-    alertNum("setFlag<"+el.textContent+">")
-    el.innerHTML= el.innerHTML.replace("<div>", "\n"+flag)
-    el.innerHTML= el.innerHTML.replace("</div>", "")
-    alertNum("!setFlag<"+el.textContent+">")
-}
-function removeFlag(params){
-    params.position = params.parent.textContent.indexOf(flag)
-    //params.parent.textContent = params.parent.textContent.replace(flag,"")
-    let child = params.parent.firstChild
-    while(child.textContent.indexOf(flag)==-1){
-        params.position -= child.textContent.length
-        child = child.nextSibling
-        alertNum(child.textContent)
-    }
-    child.textContent=child.textContent.replace(flag,"")
-    alertNum("newChild<"+child.textContent+">")
-    alertNum("newPosition="+params.position)
-    document.getSelection().collapse(child, params.position)
-    alertNum(parent.textContent)
-}
-function simpleHighlighter(el) {
-    alertNum("simpleHighlighter")
-    alertNum("REPLASE DIV")
-    alertNum(el)
-    alertNum("<"+el.innerHTML+">")
-    //предпологается что тег <div> появляется только
-    // при нажатии Enter
-    if(el.innerHTML.indexOf("<div>")!=-1){
-        alertNum("Найдена клавиша Enter!")
-        setFlag(el)
-        isEnterBt = true;
-    }
-
-    alertNum(el.innerHTML)
-    alertNum("REPLASE DIV2")
-
-    if (el instanceof Event)
-        el = this;
-    // если эту функцию назначать обработчиком input, то тут должен быть код сохранения позиции каретки...
-    let words = textToArray(el),
-        html  = '',
-        rule;
-    words.forEach(word => {
-
-        if (rule = getHlRuleFor(word))
-            html += `<span class="${rule.cl}">${rule.m}</span>`;
-        else
-            html += word;
-    });
-
-    innerHTML_replaceWithSaveCursor(el, el.innerHTML, html)
-    alertNum("UP parent = <"+el.innerHTML+">")
-    alertNum("SUCCESS")
-    // ..., а тут - код установки каретки обратно на сохраненную позицию
-}
-
-function innerHTML_replaceWithSaveCursor(elem, searchValue, replaceValue) {
-    let offset = 0;
-    if (!!getCodeArea({findActive: true})) {//существует активное окно
-        //offset = getCursorPosition(elem);
-        Cursor.saveCursorPosition(elem);
-    }
-
-    alertNum("CursorPosition = " + offset)
-    elem.innerHTML = elem.innerHTML.replace(searchValue, replaceValue)
-
-    if (!!getCodeArea({findActive: true})){//существует активное окно
-        //setCursorPosition(elem, offset)
-        Cursor.setLastCursorPosition(elem);
-    }
-}
-function getCursorPosition(parent) {
-    let selection = document.getSelection()
-    let range = new Range
-    range.setStart(parent, 0)
-    range.setEnd(selection.anchorNode, selection.anchorOffset)
-    return range.toString().length
-}
-function setCursorPosition(parent, position) {
-    alertNum("setCursorPosition")
-    alertNum("pos = "+ position)
-    let child = parent.firstChild
-
-    if(isEnterBt){
-        alertNum("WWWWWWWWWWWWWWWWWWWWWWWWWWW")
-        alertNum("parent = <"+getCodeArea({findActive:true}).textContent+">")
-        alertNum("child = <"+child.textContent+">")
-        var params={
-            parent:parent,
-            position:position
-        }
-        removeFlag(params)//передача по ссылке
-        parent = params.parent
-        position = params.position
-        alertNum("УДАЛИЛИ")
-        alertNum("parent = <"+getCodeArea({findActive:true}).textContent+">")
-        alertNum("child = <"+child.textContent+">")
-        alertNum("child length = "+child.textContent.length)
-        alertNum("parent length = "+parent.textContent.length)
-        alertNum("position = "+position)
-
-        isEnterBt=false
-    }
-    else {
-        let counter = 0
-        while (position > 0) {
-
-            let length = child.textContent.length
-            alertNum("l=" + length + "\np=" + position)
-            if (position > length) {
-                alertNum("position > length")
-                position -= length
-                alertNum("prev=" + child)
-                child = child.nextSibling
-                alertNum("next=" + child)
-                alertNum("nextSib")
-            } else {
-                counter += 1
-                if (counter > 3) return
-                alertNum("position < length")
-                alertNum("nodeType="+child.nodeType)
-                //if (child.nodeType == 3) {
-                alertNum("!child=<" + child.toString()+">")
-                alertNum(child)
-                    document.getSelection().collapse(child, position)
-                    return
-                //}
-                alertNum("child=<" + child+">")
-                //alert(child.textContent)
-                child = parent.firstChild
-                alertNum("child=" + child)
-            }
-        }
-    }
-    alertNum("setCursorPosition END")
-}
-
-
-
-
-function textToArray(el) {
-    //alertNum("textToArray")
-    //alertNum("textContent="+el.textContent)
-    //эта регулярка должна делить код на слова, которые потом ЦЕЛИКОМ всё слово можно раскрасить
-    const RE = /(^|\(+?|[^\w]+?)((?:"[^"]*")|(?:[{}\w]+))(\)+?[\S]+?)?/gmi;   // эта регулярка пропускает пробелы и переносы строки в конце
-    let result = [],
-        match, i, index=0;
-    while (match = RE.exec(el.textContent)) {
-        for (i = 1; i < match.length; i++) {
-            if (match[i]){
-                result.push(match[i]);
-                index+=match[i].length;
-                //alertNum("<"+match[i]+">")
-            }
-        }
-    }
-    for (let i = index;i<el.textContent.length;i++){
-        result.push(el.textContent[i]);
-    }
-    //alertNum("textToArray END")
-    return result;
-}
-
-function getHlRuleFor(word) {
-    alertNum("<"+word+">")
-    let rule, mr;
-
-    try {
-        if (rule = HL_RULES.find(r => mr = word.match(r.re)))
-        if(!!rule){
-            rule.m = mr[1];
-            //alertNum("<"+rule.m+">")
-        }
-    }catch (exception) {
-
-    }
-
-    return rule;
-}
-
-function alertNum(num){
-    console.log(num)
-    //проблемы с записью в конец строки
-    //проблема со вторым блоком кода
+    CodeArea.enableCode("cpp")
 }
 
 
 class Cursor {
     static lastOffset;
-    static saveCursorPosition(parentElement) {
+
+    static saveCursorPosition(keyEvent) {
         var selection = window.getSelection(),
             charCount = -1,
             node;
 
         if (selection.focusNode) {
-            if (Cursor._isChildOf(selection.focusNode, parentElement)) {
+            if (Cursor._isChildOf(selection.focusNode, codeArea)) {
                 node = selection.focusNode;
                 charCount = selection.focusOffset;
 
                 while (node) {
-                    if (node === parentElement) {
+                    if (node === codeArea) {
                         break;
                     }
-
                     if (node.previousSibling) {
                         node = node.previousSibling;
                         charCount += node.textContent.length;
@@ -264,6 +34,7 @@ class Cursor {
         }
 
         this.lastOffset = charCount;
+        CodeArea.checkKeys(keyEvent)
     }
 
     static setLastCursorPosition(element) {
@@ -319,16 +90,87 @@ class Cursor {
             }
             node = node.parentNode;
         }
-
         return false;
     }
 }
 
-/*
-* БАГИ
-* подсвечивает fuunctio
-* клавиша Enter в новой системе позиции курсора
-* если после { добавить } то удалится
-* { и } ставятся только с пробелом перед
-*
-* */
+class CodeArea{
+    static _codeArea;
+    static _enableTabKey;
+    static _hasNextSibling() {
+        return window.getSelection().focusNode.nextSibling
+    }
+    static _isLastCharacterOnNode() {
+        let selection = window.getSelection();
+        console.clear()
+        console.log(selection.focusOffset)
+        console.log(selection.focusNode.textContent.length)
+        return selection.focusOffset==selection.focusNode.textContent.length
+    }
+
+    static enableTabKey() {
+        CodeArea._enableTabKey = true;
+        window.onkeydown=function(keyEvent){
+            if(keyEvent.code == "Tab") //перехватываем tab от гнусного браузера
+                return false
+        }
+    }
+    static disableTabKey() {
+        CodeArea._enableTabKey = false;
+        window.onkeydown=function(){ }
+    }
+    static _checkTab(keyEvent) {
+        if(keyEvent.key === "Tab" && CodeArea._enableTabKey){
+            let selection = window.getSelection()
+            let node = selection.focusNode;
+            let charCount = selection.focusOffset;
+            node.textContent = CodeArea.insert(node.textContent,"\t",charCount)
+        }
+    }
+    static insert(str, substr, pos) {
+        var array = str.split('');
+        array.splice(pos, 0, substr);
+        return array.join('');
+    }
+    static _checkEnter(keyEvent) {
+        if(keyEvent.key === "Enter"){
+            let replaceString="\n"
+            if(!CodeArea._hasNextSibling()){//если последний Sibling
+                if(CodeArea._isLastCharacterOnNode()){//если курсор в конце
+                    replaceString+="\n";
+                }
+            }
+            CodeArea._codeArea.innerHTML= CodeArea._codeArea.innerHTML.replace("<div>", replaceString)
+            CodeArea._codeArea.innerHTML= CodeArea._codeArea.innerHTML.replace("</div>", "")
+            Cursor.lastOffset+=1
+        }
+    }
+    static checkKeys(keyEvent){
+        CodeArea._checkEnter(keyEvent)
+        CodeArea._checkTab(keyEvent)
+    }
+
+    static enableHighlight(){
+        hljs.highlightAll();
+        window.onkeyup=function(keyEvent){
+            CodeArea.enableTabKey()
+
+            Cursor.saveCursorPosition(keyEvent);
+            hljs.highlightAll();
+            Cursor.setLastCursorPosition(CodeArea._codeArea);
+
+            console.clear();
+        }
+    }
+    static enableCode(typeCode){
+        CodeArea._codeArea = document.getElementById("codeArea");
+        CodeArea._setAttributes(typeCode)
+        CodeArea.enableHighlight();
+        console.clear();
+    }
+    static _setAttributes(typeCode){
+        CodeArea._codeArea.setAttribute("contenteditable", "");
+        CodeArea._codeArea.setAttribute("spellcheck", "false");
+        CodeArea._codeArea.setAttribute("class", typeCode);
+    }
+}
